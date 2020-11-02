75d7929d40379164c9776e9f97a37cca09ebf7c6
Margarida Martelo <margmartelo@gmail.com>
2020-11-02 16:36:15 +0000

    Writing second and third unit tests and passing them

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,6 +4,10 @@ class Oystercard
     @balance = default_balance
   end
 
-  attr_reader :balance 
+  attr_reader :balance
+
+  def top_up(value)
+    @balance += value
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,11 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'responds to top up' do
+    expect(subject).to respond_to :top_up
+  end
+
+  it 'top_up method adds to balance' do
+    expect(subject.top_up(100)).to eq 100
+  end
 end
