bdd7f732810b368947998b0c30f26bfecf25f805
Michael Linford <linford.mike@gmail.com>
2020-11-02 16:18:37 +0000

    added balance feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,9 @@
 class Oystercard
+
+  def initialize(default_balance = 0)
+    @balance = default_balance
+  end
+
+  attr_reader :balance 
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  it 'can have a balance' do
+    expect(subject.balance).to eq 0
+  end
+
 end
